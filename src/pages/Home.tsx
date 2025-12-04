5a1f5d038cd9fd4010908dd2c57583cf73f9ba7c
Meegu80 <aceman80@naver.com>
2025-12-04 11:17:30 +0900

    12월 4일 lecture05-board 2교시 게시판목록까지 뜨게만듦(뼈대 작성 완료)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,82 @@
+import {useEffect, useState} from "react";
+import {Link} from "react-router";
+import styled from "styled-components";
+
+type Post = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+type ApiResponse = Post[];
+
+const Container = styled.div`
+    padding: 24px;
+    box-sizing: border-box;
+`;
+const Title = styled.h2`
+    margin-bottom: 20px;
+`;
+
+const List = styled.ul`
+    list-style: none;
+    padding: 0;
+`;
+
+const ListItem = styled.li`
+    padding: 16px;
+    border-bottom: 1px solid #ddd;
+
+    a {
+        text-decoration: none;
+        color: #333;
+        font-size: 18px;
+        font-weight: bold;
+
+    }
+
+`;
+
 function Home() {
+    const [loading, setLoading] = useState(true);
+    const [list, setList] = useState<Post[]>([]);
 
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then((data: ApiResponse) => {
+                setList(data);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
     return (
+        <>
+            <Container>
+                <Title>게시판</Title>
+                <List>
+                    {list.map(
+                        (item, index) => {
+                            return (
+                                <ListItem key={index}>
+                                    <Link
+                                        // to={"/detail/" + item.id}>
+                                        to={`/detail/${item.id}`}>
+                                        {item.title}
+                                    </Link>
+                                </ListItem>
+                            )
+                        }
+                    )}
+                </List>
+            </Container>
+        </>
 
-        <div>
-            Home
-        </div>
+
     );
 }
 
